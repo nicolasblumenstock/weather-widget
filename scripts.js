ad6f01e7bfc7fa42f5c75b82ab115fc2d9902bb3
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-24 23:13:46 -0400

    added tab

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	var canvas = $('#weather-canvas');
 	var context = canvas[0].getContext('2d');
 	var mainUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`
+	var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?zip=`
+	var dates = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
 
 	function animateCircle(maths,currentTemp){
 		context.fillStyle = '#582b11';
@@ -32,21 +34,44 @@ $(document).ready(function(){
 		event.preventDefault();
 		var location = $('#weather-location').val()
 		var url = `${mainUrl}${location},us&units=imperial&appid=${apiKey}`
+		var multidayUrl = `${forecastUrl}${location},us&units=imperial&cnt=5&appid=${apiKey}`
 		$.getJSON(url,(data)=>{
-			var locTemp = data.main.temp;
-			var locTemps = {
+			var currTemps = {
 				curr: data.main.temp,
 				high: data.main.temp_max,
 				low: data.main.temp_min,
 			}
-			var locLoc = data.name;
-			var locWeath = data.weather[0].description;
-			var locIcon = data.weather[0].icon + '.png';
-			animateCircle(0,locTemps.curr);
-			$('.icon').html(`<img src="http://openweathermap.org/img/w/${locIcon}" />`);
-			$('.locale').html(locLoc);
-			$('.temp-in-f').html(locTemps.curr);
-			$('.weather-type').html(locWeath);
+			var locationName = data.name;
+			var currWeather = data.weather[0].description;
+			var currIcon = data.weather[0].icon + '.png';
+			var currDate = new Date((data.dt)*1000);
+
+
+
+
+			animateCircle(0,currTemps.curr);
+
+			$('#day1').html(dates[currDate.getDay()])
+			$('.icon').html(`<img src="http://openweathermap.org/img/w/${currIcon}" />`);
+			$('.locale').html(locationName);
+			$('.temp-in-f').html(currTemps.curr);
+			$('.weather-type').html(currWeather);
+
+
 		})
+
+		$.getJSON(multidayUrl,(otherData)=>{
+			for (let i = 1; i < otherData.list.length; i++){
+				var dailyMin = otherData.list[i].temp.min;
+				var dailyMax = otherData.list[i].temp.max;
+				var dailyWeather = otherData.list[i].weather[0].main;
+				var dailyIcon = otherData.list[i].weather[0].icon;
+
+				$('#tabday'+(i+1)).html("hello. is it me you're looking for.")
+			}
+		})
+
 	})
+
+
 })
